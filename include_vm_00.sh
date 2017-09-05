0729a237e53d2f5b890c6e9ad6600e127a6e92bd
Andrew McNab <andrew.mcnab@cern.ch>
2017-09-05 19:36:29 +0100

    Add includes to user_data_vm

---
+++ include_vm_00.sh
@@ -0,0 +1,25 @@
+##
+## Added for SKA/LOFAR
+##
+cat <<EOF >/etc/cvmfs/config.d/softdrive.nl.conf
+CVMFS_SERVER_URL='http://cvmfs01.nikhef.nl/cvmfs/@fqrn@;http://cvmfs-egi.gridpp.rl.ac.uk:8000/cvmfs/@fqrn@'
+CVMFS_PUBLIC_KEY=/etc/cvmfs/keys/softdrive.nl.pub
+EOF
+
+cat <<EOF >/etc/cvmfs/keys/softdrive.nl.pub
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA481/kCXbrVtLuzcFZ2uO
+EmiAKx28qXIkonPwr/gSmqQ8k1zQA7dKK5YZwZSbVwgYqvhvW6i3vKWLGVDj+elH
+1u8uumPzzlAJHrS1XoR8rY4xUULjQBvV9HuJxE6OK4ZEZPvQmeGmjXd446c8J5cv
+BQFtaonRnrxAbtO+Z0KtzsNOzBNFegu9z+lT7/fxV17Qh10w5IKQjm/v6jPdj1ME
+CrG4QW2S9+Y+7YzbRP5QYaE4cl5cBI3Yb048ufgLJMfX3++uqwGM+rqNs/CzHvsW
+dO6Jznr9EbzqbIrTsFeUThNmsGPObxOT3VmB0BTTjrZSYjgf8oEE4hdhgNQgh7vs
+OwIDAQAB
+-----END PUBLIC KEY-----
+EOF
+
+cvmfs_config reload
+##
+## End of added for SKA/LOFAR
+##
+
