06133d0561a992e23aa43695148c6392c1a201ab
Andrew McNab <andrew.mcnab@cern.ch>
2018-06-26 12:10:10 +0100

    More universalisation

--- include_vm_01.sh
+++ include_vm_01.sh
@@ -1,2 +1,2 @@
 # UnpackDev unpacks dev.tgz over the installed DIRAC tree
-#SUBMIT_POOL_OPTS="$SUBMIT_POOL_OPTS --commandExtensions dev --commands 'CheckWorkerNode,InstallDIRAC,UnpackDev,ConfigureBasics,CheckCECapabilities,CheckWNCapabilities,ConfigureSite,ConfigureArchitecture,ConfigureCPURequirements,MultiLaunchAgent'"
+#export SUBMIT_POOL_OPTS="$SUBMIT_POOL_OPTS --commandExtensions dev --commands 'CheckWorkerNode,InstallDIRAC,UnpackDev,ConfigureBasics,CheckCECapabilities,CheckWNCapabilities,ConfigureSite,ConfigureArchitecture,ConfigureCPURequirements,MultiLaunchAgent'"
